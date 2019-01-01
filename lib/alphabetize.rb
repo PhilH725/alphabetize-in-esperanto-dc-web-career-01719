de2ca2ef7523012bc89280bba3a8c82a9dcd2c53
Phil Hartley <philh725@gmail.com>
2019-01-01 20:56:25 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,14 +1,8 @@
-#def alphabetize(arr)
-  # code here
-  
-  #alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
-  
- # arr.sort_by { |alphabet| alphabet }
-  
-#end
 
 def alphabetize(arr)
   
-  puts arr.sort_by { |string| string.length}[-1]
+  alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  
+  arr.sort_by { |alphabet| alphabet }
   
 end
