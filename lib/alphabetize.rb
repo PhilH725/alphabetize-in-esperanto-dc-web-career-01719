0fbadbc59520eb48a58643e0428b126a254262ad
Phil Hartley <philh725@gmail.com>
2019-01-01 20:30:16 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -2,15 +2,6 @@ def alphabetize(arr)
   # code here
   a = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".split(//)
   
-  alpha_array = []
-  a.each do |letter|
-    arr.each do |string|
-      if string[0] == letter
-        alpha_array << string
-        arr.delete(string)
-      end
-    end
-  end
-  puts alpha_array
-  alpha_array
+  arr.sort_by
+  
 end
