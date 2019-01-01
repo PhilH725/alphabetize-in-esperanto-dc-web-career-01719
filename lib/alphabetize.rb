a84020e90947a8d05a5ebe4cca22269a282c86bb
Phil Hartley <philh725@gmail.com>
2019-01-01 21:04:52 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -3,9 +3,9 @@ def alphabetize(arr)
   
   alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   
-  ascii = "@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\"
+  ascii_chars = "@ABCDEFGHIJKLMNOPQRSTUVWXYZ[]"
   
   #arr.sort_by { |alphabet| alphabet }
-  arr.sort_by{|string| string.tr(alphabet, ascii)}
+  arr.sort_by{|string| string.tr(alphabet, ascii_chars)}
   
 end
